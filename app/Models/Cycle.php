05e93b0c74eb1aa7f67065281bccd6e662b449d7
Mecene Phrygien <mecenephrygien@gmail.com>
2023-02-22 12:59:38 +0300

    gestion cycle

--- app/Models/Cycle.php
+++ app/Models/Cycle.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Cycle extends Model
 {
     use HasFactory;
+
+    protected $table = "cycles";
+
+    protected $fillable = [
+        'cycle_name',
+        'cycle_code',
+        'ecole_id',
+        'annee_id'
+    ];
+
+    public function ecole()
+    {
+        return $this->belongsTo(Ecole::class, 'ecole_id');
+    }
+
+    public function anneescolaire()
+    {
+        return $this->belongsTo(Anneescolaire::class, 'annee_id');
+    }
 }
