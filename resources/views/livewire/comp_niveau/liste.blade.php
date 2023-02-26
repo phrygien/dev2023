ba510a8116a452a7db3328430c6567b43cedb61d
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-02-26 16:23:36 +0300

    Update liste.blade.php

--- resources/views/livewire/comp_niveau/liste.blade.php
+++ resources/views/livewire/comp_niveau/liste.blade.php
@@ -293,7 +293,7 @@
                     <th width="220px"
                     class="whitespace-nowrap bg-slate-200 px-4 py-3 font-semibold uppercase text-slate-800 dark:bg-navy-800 dark:text-navy-100 lg:px-5"
                     >
-                    Deadline
+                    {{ __('Action') }}
                     </th>
                 </tr>
                 </thead>
