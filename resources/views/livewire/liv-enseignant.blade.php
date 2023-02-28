41a07ebd7f4b1ef8b4c1352c538fdb8cad244972
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-02-28 14:12:57 +0300

    Update liv-enseignant.blade.php
    modification class boutton

--- resources/views/livewire/liv-enseignant.blade.php
+++ resources/views/livewire/liv-enseignant.blade.php
@@ -40,7 +40,7 @@
             </div>
             <div class="flex justify-center space-x-2">
             <button wire:click="cancelCreate()"
-                    class="btn min-w-[7rem] border border-slate-300 font-medium text-slate-700 hover:bg-slate-150 focus:bg-slate-150 active:bg-slate-150/80 dark:border-navy-450 dark:text-navy-100 dark:hover:bg-navy-500 dark:focus:bg-navy-500 dark:active:bg-navy-500/90">
+            class="btn bg-error/10 font-medium text-error hover:bg-error/20 focus:bg-error/20 active:bg-error/25">
                     {{ __('annuler')}}
                 </button>
                 <form wire:submit.prevent="store" enctype="multipart/form-data">
@@ -52,7 +52,7 @@
                 </div>
                 <div wire:loading.remove>
                 <button
-                    class="btn min-w-[7rem] bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90">
+                class="btn bg-primary/10 font-medium text-primary hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-accent-light/10 dark:text-accent-light dark:hover:bg-accent-light/20 dark:focus:bg-accent-light/20 dark:active:bg-accent-light/25">
                     {{ __('enregistrer')}}
                 </button>
                 </div>
